4bef7c100389b768c6eda77eb283d6566801fa49
ziyu-0123 <ziyuliu123@qq.com>
2026-08-25 17:47:16 +0800

    feat: create QuestionRadio component and integrate into page

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,40 +1,58 @@
-import Head from 'next/head'
-import type { GetServerSidePropsContext } from 'next'
+import Head from "next/head";
+import type { GetServerSidePropsContext } from "next";
 
 type PropsType = {
-  id: string
-}
+  id: string;
+};
 
 // 临时引用
-import QuestionInput from '@/components/QuestionComponents/QuestionInput'
+import QuestionInput from "@/components/QuestionComponents/QuestionInput";
+import QuestionRadio from "@/components/QuestionComponents/QuestionRadio";
 
 export default function Question(props: PropsType) {
-  return <>
-    <Head>
-      <title>Question</title>
-      <meta name="description" content="question page" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main>
-      <h1>Question page</h1>
-      <p>{props.id}</p>
+  return (
+    <>
+      <Head>
+        <title>Question</title>
+        <meta name="description" content="question page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Question page</h1>
+        <p>{props.id}</p>
 
-      <form>
-        <QuestionInput fe_id="c1" props={{ title: '你的姓名', placeholder: '请输入姓名' }}/>
-      </form>
-    </main>
-  </>
+        <form>
+          <QuestionInput
+            fe_id="c1"
+            props={{ title: "你的姓名", placeholder: "请输入姓名" }}
+          />
+          <QuestionRadio
+            fe_id="c2"
+            props={{
+              title: "你的性别",
+              options: [
+                { value: "male", text: "男" },
+                { value: "female", text: "女" },
+              ],
+              value: "male",
+              isVertical: false,
+            }}
+          />
+        </form>
+      </main>
+    </>
+  );
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { id = '' } = context.params as { id?: string }
+  const { id = "" } = context.params as { id?: string };
 
   // 根据 id await 获取问卷数据
 
   return {
     props: {
       id,
-    }
-  }
+    },
+  };
 }
